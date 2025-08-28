eec6811828e6f32bf6f791dbdb08b52cf296d22c
zachary-hardin <zbhardin@gmail.com>
2025-08-28 14:12:04 -0500

    Remove Weather

--- ParksService/ParksService/Program.cs
+++ ParksService/ParksService/Program.cs
@@ -28,25 +28,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.MapGet("/get-parks", async (IParkRepository parkRepository) =>
 {
     var parks = await parkRepository.GetAllAsync();
